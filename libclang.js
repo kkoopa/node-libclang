80c5bfe22a90ade057d06dd6ce72e862e2056ba4
Benjamin Byholm <bbyholm@abo.fi>
2015-06-02 03:14:16 +0300

    Add Range to exports

--- libclang.js
+++ libclang.js
@@ -20,6 +20,7 @@
 
 exports.Cursor = require('./lib/cursor');
 exports.Index = require('./lib/index');
+exports.Range = require('./lib/range');
 exports.TranslationUnit = require('./lib/translationunit');
 exports.Type = require('./lib/type');
 exports.Location = require('./lib/location');
