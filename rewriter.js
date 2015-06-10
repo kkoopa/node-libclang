2367297dfd9082543c753ec9b7a679360940b27c
Benjamin Byholm <bbyholm@abo.fi>
2015-06-10 22:28:45 +0300

    Rewriter computes deltas somewhat correctly.

--- rewriter.js
+++ rewriter.js
@@ -16,6 +16,7 @@ Array.prototype.bisect_left = function (val, comparator) {
 
 var offsets = [];
 
+/* TODO: this is too messy and unnecessarily complicated */
 function getDelta(offset) {
 	var length = offsets.length, idx;
 
@@ -23,38 +24,29 @@ function getDelta(offset) {
 		return 0;
 	}
 	idx = offsets.bisect_left(offset, function (a, b) { return a[0] - b; }) - 1;
+	idx = idx === -1 ? 0 : idx;
 	return offsets[idx][1];
 }
 
+/* TODO: this is too messy and unnecessarily complicated */
 function setDelta(offset, delta) {
 	var length = offsets.length, idx;
 
-	console.log('setting delta for', offset);
-
 	if (length === 0) {
 		offsets.push([offset, delta]);
-		console.log('to', delta);
 	} else {
 		idx = offsets.bisect_left(offset, function (a, b) { return a[0] - b; });
 		if (idx < length && offsets[idx][0] === offset) {
-			console.log('was', offsets[idx][1]);
-			console.log('delta is', delta);
-			offsets[idx][1] = delta;
-			console.log('to', offsets[idx][1]);
+			if (idx > 0) {
+				offsets[idx][1] = delta + offsets[idx][1] - offsets[idx - 1][1];
+			} else {
+				offsets[idx][1] = delta + offsets[idx][1];
+			}
 		} else {
 			offsets.push([offset, delta]);
-			console.log('to', delta);
 		}
 	}
-
-	console.log(offsets);
 }
-
-/*console.log(getDelta(5));
-console.log(getDelta(6));
-console.log(getDelta(2));
-console.log(getDelta(10));
-console.log(getDelta(15));*/
 
 var source = 'NanAssignPersistent(persistent, args[0]);'
 
@@ -63,9 +55,7 @@ function makeDelete(offset, length) {
 
   return function (source) {
     var result, delta = getDelta(offset);
-    console.log('delta', delta);
     setDelta(offset, delta - length);
-    console.log('Delete');
     result = source.substring(0, offset + delta) + source.substring(offset + delta + length);
     return result;
   };
@@ -76,10 +66,7 @@ function makeInsert(offset, string) {
 
   return function (source) {
     var result, delta = getDelta(offset);
-    console.log('delta', delta);
-    console.log('stringlength', string.length);
     setDelta(offset, delta + string.length);
-    console.log('Insert');
     result = source.substring(0, offset + delta) + string + source.substring(offset + delta);
     return result;
   };
@@ -89,15 +76,11 @@ function makeReplace(offset, length, string) {
   'use strict';
 
   return function (source) {
-    console.log('Replace');
     return makeInsert(offset, string)(makeDelete(offset, length)(source));
   };
 }
 
 source = makeDelete(0, 'NanAssignPersistent('.length)(source);
 source = makeReplace('NanAssignPersistent(persistent'.length, ', '.length, '.Reset(')(source);
-source = makeDelete('NanAssignPersistent(persistent, '.length, 'args'.length)(source);
-console.log(source);
-source = makeInsert('NanAssignPersistent(persistent, '.length, 'info')(source);
-//source = makeReplace('NanAssignPersistent(persistent, '.length, 'args'.length, 'info')(source);
+source = makeReplace('NanAssignPersistent(persistent, '.length, 'args'.length, 'info')(source);
 console.log(source);
